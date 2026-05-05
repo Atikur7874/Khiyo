bffb3af8a9e6de19ae92190f4e7119e5d6cac36f
Atikur7874 <53107817+Atikur7874@users.noreply.github.com>
2026-05-06 01:44:50 +0600

    fix Vercel import resolution

--- frontend/src/components/Common/Header.jsx
+++ frontend/src/components/Common/Header.jsx
@@ -1,7 +1,6 @@
 import React from "react";
-import TopBar from "../Layout/TopBar";
-import NavBar from "../Layout/NavBar";
-
+import TopBar from "../Layout/TopBar.jsx";
+import NavBar from "../Layout/NavBar.jsx";
 const Header = () => {
   return (
     <div>
